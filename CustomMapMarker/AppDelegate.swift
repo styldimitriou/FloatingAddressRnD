cb0a467ed68d6eac0cb16067e88c6c22bf810c79
Nikos Maounis <nikospsy2k@gmail.com>
2020-06-02 09:25:41 +0300

    Fix styling

--- CustomMapMarker/AppDelegate.swift
+++ CustomMapMarker/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         GMSServices.provideAPIKey("AIzaSyBq2Y5ZiQaKxQ00Fg6d2EScuK8nHYs9aP4")
+        
         return true
     }
 
